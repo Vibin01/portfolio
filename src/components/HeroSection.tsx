90787b1b20eca8c242e1e23754d46e974ea3e784
Vibin01 <vibinakashv2001@gmail.com>
2025-07-22 22:26:53 +0530

    Added hero section and name

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const HeroSection = () => {
-  return <></>;
+  return (
+    <>
+      <section className="font-figtree flex justify-center text-white">
+        <article className="w-1/2 flex flex-col justify-center items-center gap-4 md:text-[64px] font-bold">
+          {" "}
+          <h1>Hello. </h1> <h2 className="font-normal">I'm Vibin</h2>
+          <h2 className="font-playfair">Web Developer</h2>
+        </article>
+        <article className="w-1/2"></article>
+      </section>
+    </>
+  );
 };
 
 export default HeroSection;
